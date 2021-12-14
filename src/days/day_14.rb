73ef0c0fb402206f60efbc988798850d70537850
Rob <robliddle@gocardless.com>
2021-12-14 21:55:56 +0000

    Non memory happy alternative

--- src/days/day_14.rb
+++ src/days/day_14.rb
@@ -8,79 +8,68 @@ class Day
     parts = input.split("\n\n")
 
     template = parts[0].strip.chars
-    pairs = parts[1].lines.reduce({}) do |acc, line|
+    pair_rules = parts[1].lines.reduce({}) do |acc, line|
       pair, insert = line.split(" -> ").map(&:strip)
-      acc[pair] = insert
+
+      # When inserted you get 2 pairs essentially added
+      acc[pair] = [pair[0] + insert, insert + pair[1]]
       acc
     end
 
-    [template, pairs]
+    [template, pair_rules]
   end
 
   def a input
-    template, pairs = parse_input(input)
+    template, pair_rules = parse_input(input)
 
-    # Go through the template X times inserting matches in
-    template = inject_pairs(10, template, pairs)
+    poly = Hash.new(0)
+    template.each_cons(2) {|char_pair| poly[char_pair.join] += 1 }
 
-    # Now need to count the elements
-    groups = template.reduce(Hash.new(0)) {|acc, ele| acc[ele] += 1; acc }
-
-    max_key, max_val = groups.max_by {|k,v| v }
-    min_key, min_val = groups.min_by {|k,v| v }
-
-    max_val - min_val
+    poly = inject_pairs(10, poly, pair_rules)
+    count_results(poly, template)
   end
 
   def b input
-    template, pairs = parse_input(input)
+    template, pair_rules = parse_input(input)
 
-    # Go through the template X times inserting matches in
-    template = inject_pairs(40, template, pairs)
+    poly = Hash.new(0)
+    template.each_cons(2) {|char_pair| poly[char_pair.join] += 1 }
 
-    # Now need to count the elements
-    groups = template.reduce(Hash.new(0)) {|acc, ele| acc[ele] += 1; acc }
-
-    max_key, max_val = groups.max_by {|k,v| v }
-    min_key, min_val = groups.min_by {|k,v| v }
-
-    max_val - min_val
+    poly = inject_pairs(40, poly, pair_rules)
+    count_results(poly, template)
   end
 
-  def inject_pairs x, template, pairs
-    x.times do
-      temp_bucket = []
-
-      template.each_cons(2) do |char_pair|
-        match = pairs[char_pair.join]
-
-        # always put the first in.
-        temp_bucket << char_pair[0]
-
-        # Add the middle insert if we have a match
-        temp_bucket << match if match
-      end
-
-      # at the end we will be missing the last char, chuck it on
-      temp_bucket << template.last
-
-      # Override the template for this round!
-      template = temp_bucket
+  def count_results poly, template
+    counts = Hash.new(0)
+    poly.each do |pair, count|
+      counts[pair[0]] += count
+      counts[pair[1]] += count
     end
 
-    template
+    # the first and last character will have not been counted twice?
+    counts[template.last] += 1
+    counts[template.first] += 1
+
+    sorted_counts = counts.values.sort
+
+    (sorted_counts.last - sorted_counts.first) / 2
+  end
+
+  def inject_pairs x, poly, pair_rules
+    x.times do
+      newpoly = Hash.new(0)
+
+      # Iterate over the current pairs
+      poly.each do |pair, count|
+        pairs_created = pair_rules[pair]
+
+        newpoly[pairs_created[0]] += count
+        newpoly[pairs_created[1]] += count
+      end
+
+      poly = newpoly
+    end
+
+    poly
   end
 end
-
-###
-# ""
-# "NBBBCNCCNBBNBNBBCHBHHBCHB"
-# "NBBNBNBBCCNBCNCCNBBNBBNBBBNBBNBBCBHCBHHNHCBBCBHCB"
-#
-# Template:     NNCB
-# After step 1: NCNBCHB
-#               NCNBCHB
-# After step 2: NBCCNBBBCBHCB
-#               NBCCNBBBCBHCB
-# After step 3: NBBBCNCCNBBNBNBBCHBHHBCHB
-# After step 4: NBBNBNBBCCNBCNCCNBBNBBNBBBNBBNBBCBHCBHHNHCBBCBHCB
